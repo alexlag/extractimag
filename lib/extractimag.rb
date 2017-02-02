dbb8fc08551f87e684d96d07630a68dea4079fee
alexlag <bo4onok@gmail.com>
2017-02-02 13:32:26 +0300

    working images extractor

--- lib/extractimag.rb
+++ lib/extractimag.rb
@@ -1,5 +1,21 @@
+require 'httparty'
+require 'nokogiri'
 require 'extractimag/version'
 
 module Extractimag
-  # Your code goes here...
+  include ::HTTParty
+
+  parser proc { |data| ::Nokogiri::HTML(data) }
+
+  def self.images(url)
+    doc = get(url).parsed_response
+    extract_images(doc, url)
+  end
+
+  def self.extract_images(doc, base_url)
+    doc
+      .css('img')
+      .map { |el| el['src'] }
+      .map { |rel| URI.join(base_url, rel).to_s }
+  end
 end
--- spec/extractimag_spec.rb
+++ spec/extractimag_spec.rb
@@ -1,11 +1,26 @@
+require 'nokogiri'
 require 'spec_helper'
+
+ONE = Nokogiri::HTML('<html><body><img src="/1.png"></body></html>').freeze
+NONE = Nokogiri::HTML('<html><body></body></html>').freeze
 
 describe Extractimag do
   it 'has a version number' do
     expect(Extractimag::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'extracts some images from good urls' do
+    expect(Extractimag.images('https://google.com')).to be_kind_of Array
+    expect(Extractimag.images('http://yandex.ru')).to be_kind_of Array
+  end
+
+  it 'extracts images from String data' do
+    expect(Extractimag.send(:extract_images, ONE, 'http://foo.org')).to eq ['http://foo.org/1.png']
+    expect(Extractimag.send(:extract_images, NONE, 'http://foo.org')).to eq []
+  end
+
+  it 'throws exception with bad urls' do
+    expect { Extractimag.images('a') }.to raise_error Errno::ECONNREFUSED
+    expect { Extractimag.images('a:/b') }.to raise_error "'a:/b' Must be HTTP, HTTPS or Generic"
   end
 end
